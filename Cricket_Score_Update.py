2618ffde3e3164aa736a63c10f89ff9b6a8a9c6e
Shiv1202 <shivansht9211@gmail.com>
2020-03-08 15:44:26 +0530

    File Added.

---
+++ Cricket_Score_Update.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+from time import sleep
+
+url = "http://static.cricinfo.com/rss/livescores.xml"
+while True:
+    r = requests.get(url)
+    while r.status_code is not 200:
+        r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    data = soup.find_all('description')
+    for i in data:
+        if "India" in str(i.text):
+            socre = i.text
+            print(socre)
+            break
+    break
+ 
